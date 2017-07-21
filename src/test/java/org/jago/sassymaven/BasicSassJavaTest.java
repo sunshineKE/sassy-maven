9d97ed442e8e1d869ce66e4349b9533ba1239874
Claudia Hirt <hirt-claudia@gmx.de>
2017-07-21 19:58:05 +0200

    Add sass-java dependency, first test

---
+++ src/test/java/org/jago/sassymaven/BasicSassJavaTest.java
@@ -0,0 +1,11 @@
+package org.jago.sassymaven;
+
+import org.junit.Test;
+
+public class BasicSassJavaTest {
+	
+	@Test
+	public void testSassCompiler(){
+		System.out.println("Start test SASS compiler");
+	}
+}
