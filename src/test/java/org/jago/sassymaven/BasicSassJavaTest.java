d68076dbe431b2b0501b5d4aa1a86ea7d5a07655
Stefan Beck <beck@katie.aves-verticis>
2017-07-21 20:34:19 +0200

    Add sample test

--- src/test/java/org/jago/sassymaven/BasicSassJavaTest.java
+++ src/test/java/org/jago/sassymaven/BasicSassJavaTest.java
@@ -1,11 +1,30 @@
 package org.jago.sassymaven;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.Test;
+
+import com.cathive.sass.SassCompilationException;
+import com.cathive.sass.SassContext;
+import com.cathive.sass.SassFileContext;
 
 public class BasicSassJavaTest {
 	
 	@Test
 	public void testSassCompiler(){
 		System.out.println("Start test SASS compiler");
+		Path srcRoot = Paths.get("./src/test/resources");
+		SassContext ctx = SassFileContext.create(srcRoot.resolve("sassTestfile.scss"));
+		try {
+            ctx.compile(System.out);
+        } catch (SassCompilationException e) {
+            // Will print the error code, filename, line, column and the message provided
+            // by libsass to the standard error output.
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(String.format("Compilation failed: %s", e.getMessage()));
+        }
 	}
 }
