d3f5240146bf321d3940db13a384455b8d650c30
Stefan Beck <beck@katie.aves-verticis>
2017-07-24 23:19:35 +0200

    Add n-to-n test, restructure

--- src/test/java/org/jago/sassymaven/mojo/CompileTimeSassyMojoTest.java
+++ src/test/java/org/jago/sassymaven/mojo/CompileTimeSassyMojoTest.java
@@ -18,20 +18,23 @@ public class CompileTimeSassyMojoTest /* extends AbstractMojoTestCase */ {
 	@Rule
 	public MojoRule rule = new MojoRule();
 
+	private void runCompiler(File pom) throws MojoExecutionException, MojoFailureException, Exception {
+		CompileTimeSassyMojo ctMojo = new CompileTimeSassyMojo();
+		ctMojo = (CompileTimeSassyMojo) rule.configureMojo(ctMojo,
+				rule.extractPluginConfiguration("sassy-maven-plugin", pom));
+
+		ctMojo.execute();
+	}
+
 	@Test
 	public void executeCompileTimeMojo1to1() {
 
-		File pom = new File("src/test/resources/integrationtest/1to1/pom_1to1.xml");
+		File pom = new File("src/test/resources/integrationtest/1-to-1/pom-1-to-1.xml");
 		Assert.assertNotNull(pom);
 		Assert.assertTrue(pom.exists());
 
 		try {
-			
-			CompileTimeSassyMojo ctMojo = new CompileTimeSassyMojo();
-			ctMojo = (CompileTimeSassyMojo) rule.configureMojo(ctMojo,
-					rule.extractPluginConfiguration("sassy-maven-plugin", pom));
-
-			ctMojo.execute();
+			runCompiler(pom);
 		}
 		catch (MojoExecutionException e) {
 			Assert.assertNull(e);
@@ -41,8 +44,33 @@ public class CompileTimeSassyMojoTest /* extends AbstractMojoTestCase */ {
 			Assert.assertNull(e);
 		}
 
-		File compiledFile = new File("src/test/resources/integrationtest/1to1/output/sassTestFile.css");
+		File compiledFile = new File("src/test/resources/integrationtest/1-to-1/output/sassTestFile.css");
 
 		Assert.assertTrue(compiledFile.exists());
 	}
+
+	@Test
+	public void executeCompileTimeMojo_n_to_n() {
+
+		File pom = new File("src/test/resources/integrationtest/n-to-n/pom-n-to-n.xml");
+		Assert.assertNotNull(pom);
+		Assert.assertTrue(pom.exists());
+
+		try {
+			runCompiler(pom);
+		} catch (MojoExecutionException e) {
+			Assert.assertNull(e);
+		} catch (MojoFailureException e) {
+			Assert.assertNull(e);
+		} catch (Exception e) {
+			Assert.assertNull(e);
+		}
+
+		File compiledFile1 = new File("src/test/resources/integrationtest/n-to-n/output1/sassTestFile.css");
+		File compiledFile2 = new File("src/test/resources/integrationtest/n-to-n/output2/sassTestFile.css");
+
+		Assert.assertTrue(compiledFile1.exists());
+		Assert.assertTrue(compiledFile2.exists());
+	}
+
 }
