41db96c996237c6c7c675600f73125005af1efe3
Claudia Hirt <hirt-claudia@gmx.de>
2017-07-25 21:34:07 +0200

    Shorten goal name - execution can now  be done with sassy:update

--- src/main/java/org/jago/sassymaven/mojos/CompileTimeSassyMojo.java
+++ src/main/java/org/jago/sassymaven/mojos/CompileTimeSassyMojo.java
@@ -4,7 +4,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
-@Mojo(name = "update:stylesheets")
+@Mojo(name = "update")
 public class CompileTimeSassyMojo extends AbstractSassyMojo {
 
 	@Override
